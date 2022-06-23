83ce6efe5e37483d5ad980c9d55d5f5c68410831
Max G <zmiycheg@gmail.com>
2022-06-23 23:05:38 +0300

    Initial commit

---
+++ coin.rb
@@ -0,0 +1,16 @@
+puts 'Бросаем монетку'
+
+  line = "o-o-o-o-o-o-o-o-o-o-o-o-o-o".each_char do |line|
+    sleep 0.02
+    print line
+  end
+
+puts
+
+  if rand(11) == 10
+    puts 'Монетка встала на ребро'
+  elsif rand(2) == 1
+    puts 'Выпал орел'
+  else
+    puts 'Выпала решка'
+  end
